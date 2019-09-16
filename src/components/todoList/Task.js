038ccec738e1a0bf5123a6ce06526b225518064b
Dmytro Ursul <cartobus@gmail.com>
2019-09-16 23:19:17 +0300

    add edit task input

--- src/components/todoList/Task.js
+++ src/components/todoList/Task.js
@@ -3,13 +3,31 @@ import { connect } from 'react-redux'
 import { removeTask } from './actions'
 
 class Task extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      isEditing: false,
+      name: props.name
+    }
+  }
+
+  onChange = (event) => {
+    this.setState({name: event.target.value})
+  }
+
+  editTask = () => {
+    this.setState({isEditing: true})
+  }
+
   render() {
     let { name, id, removeTask } = this.props
 
     return (
       <li>
-        <p className="task">{ name }</p>
-        <span clasName="remove-item" onClick={ () => removeTask(id) }>x</span>
+          { this.state.isEditing ? <input className="editTask" value={this.state.name} onChange={this.onChange}/>
+                                 : <p className="task" onClick={this.editTask}>{ name }</p> }
+        <span className="remove-item" onClick={ () => removeTask(id) }>x</span>
       </li>
     )
   }
