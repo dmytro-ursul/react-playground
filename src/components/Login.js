557fd2780f7244f42c07bffd0bfe3139d03b3394
Dmytro Ursul <cartobus@gmail.com>
2022-08-11 21:18:11 +0300

    submit form and redirect

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,39 @@
 import React from 'react';
 
-function Login() {
-  return (
-    <div>
-      <form action="/session" method="post">
-        <div className="form-group">
-          <input type="text" name="username" placeholder="username"/>
-        </div>
-        <div className="form-group">
-          <input type="password" name="password" placeholder="password"/>
-        </div>
-        <input className="btn btn-primary" type="submit" value="login"/>
-      </form>
-    </div>
-  );
+class Login extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        window.location.href = "/";
+      } else {
+        alert("Login failed");
+      }
+    };
+    xhr.send(data);
+  }
+
+  render() {
+    return (
+      <div>
+        <form action="http://localhost:3051/sessions" method="post" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <input type="text" name="username" placeholder="username"/>
+          </div>
+          <div className="form-group">
+            <input type="password" name="password" placeholder="password"/>
+          </div>
+          <input className="btn btn-primary" type="submit" value="login"/>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default Login
