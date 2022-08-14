3ad1426d9477ad05246bd1ce03437d52f1ed8373
Dmytro Ursul <cartobus@gmail.com>
2022-08-14 12:06:27 +0300

    add rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
   after_action :cors_set_access_control_headers
 
@@ -5,6 +7,6 @@ class ApplicationController < ActionController::API
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-    headers['Access-Control-Max-Age'] = "1728000"
+    headers['Access-Control-Max-Age'] = '1728000'
   end
 end
