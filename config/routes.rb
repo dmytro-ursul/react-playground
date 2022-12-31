f9edf91cb9ab9cea7a0cb596b919ab909175d0bd
Dmytro Ursul <cartobus@gmail.com>
2022-12-31 12:44:41 +0200

    upgrade to ruby 3.1.2

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,4 @@ Rails.application.routes.draw do
 
   post "/graphql", to: "graphql#execute"
   resources :sessions, only: [:create]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
