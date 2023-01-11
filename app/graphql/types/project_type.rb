5c4cc01f32e997605a8a618b2a0d044ec768ecf7
Dmytro Ursul <cartobus@gmail.com>
2023-01-11 09:22:42 +0200

    add resolvers for project type

--- app/graphql/types/project_type.rb
+++ app/graphql/types/project_type.rb
@@ -2,8 +2,10 @@
 
 module Types
   class ProjectType < Types::BaseObject
+    description "A project"
+
     field :id, ID, null: false
-    field :name, String
+    field :name, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :tasks, [TaskType], null: false
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,5 +7,17 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
     field :projects, [ProjectType], 'Returns all projects', null: false
+
+    field :project, ProjectType, 'Returns a project', null: false do
+      argument :id, ID, required: true
+    end
+
+    def projects
+      Project.all
+    end
+
+    def project(id:)
+      Project.find(id)
+    end
   end
 end
